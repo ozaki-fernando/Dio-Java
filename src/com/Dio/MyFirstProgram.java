51934028d70e21cdac9c699727cd71c5d20eaf2b
ozaki-fernando <ozak.pgm@gmail.com>
2021-12-13 23:02:24 -0300

    Inclusão de primeiro programa java

---
+++ src/com/Dio/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.Dio;
+
+import com.Dio.base.Order;
+
+public class MyFirstProgram{
+    public static void main(String[] args){
+        final Order order = new Order("2234688880");
+        System.out.println(order);
+    };
+};
