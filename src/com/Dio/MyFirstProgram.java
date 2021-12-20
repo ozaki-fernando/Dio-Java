28ccf9c2c7ea0dc496afccecab13878506bb46e8
ozaki-fernando <ozak.pgm@gmail.com>
2021-12-19 22:48:06 -0300

    Inclus]ao de estruturas de repetição

--- src/com/Dio/MyFirstProgram.java
+++ src/com/Dio/MyFirstProgram.java
@@ -4,7 +4,7 @@ import com.Dio.base.Order;
 
 public class MyFirstProgram{
     public static void main(String[] args){
-        final Order order = new Order("2234688880");
+        final Order order = new Order("2234688880", 400);
         System.out.println(order);
     };
 };
--- src/com/Dio/base/Order.java
+++ src/com/Dio/base/Order.java
@@ -2,11 +2,26 @@ package com.Dio.base;
 
 public class Order{
     private final String code;
-    public Order(String code){
+    private final int totalValue;
+    double total;
+
+    public Order(String code, int totalValue){
         this.code = code;
+        this.totalValue = totalValue;
+    }
+    
+    public double calculateFee(){
+        switch (this.totalValue){
+            case 100:
+                return totalValue * 0.99;
+            case 200:
+                return totalValue * 1.99;
+            default:
+                return totalValue;
+        }
     }
     @Override
     public String toString(){
-        return "Order= code:'" + code + "'.";
+        return "Order= code:'" + code + "'. Valor = " + this.totalValue + ".";
     }
 };
